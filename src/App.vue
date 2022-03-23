54a92908e5a6eac8d90b829bc29a37091ccaf6d6
Alexander Bychkov <alx.bychkov@gmail.com>
2022-03-23 19:27:35 +0300

    init and start project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <Portfolio />
+  </div>
+</template>
+
+<script>
+import Portfolio from './views/Portfolio/Portfolio.vue';
+
+export default {
+  name: 'App',
+  components: {Portfolio}
+}
+</script>
